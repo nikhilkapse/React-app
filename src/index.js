e0cdd1704824ed8d89b3b92a2dd757a16f7543e6
nikhilkapse <38915415+nikhilkapse@users.noreply.github.com>
2019-07-15 11:57:25 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+
+// render(<App />, document.getElementById('root'))
+
+render(
+  <h1>
+    <center>Hello World</center>
+  </h1>,
+  document.getElementById("root")
+);
---
+++ src/instructions.md
@@ -0,0 +1,4 @@
+# Instructions
+Create a component that says "Hello World!" and render it into the DOM.
+
+See the final result [here](https://o7wpw8mxnq.codesandbox.io/).
